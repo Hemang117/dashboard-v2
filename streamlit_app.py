322b08e3190ecdbde00f6aa1e3bb3053f6cbd540
Hemang117 <hemangsaini46600@gmail.com>
2024-03-23 23:57:22 +0530

    ok

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,10 @@ import plost
 
 st.set_page_config(layout='wide', initial_sidebar_state='expanded')
 
+
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
     
 st.sidebar.header('Dashboard `version 2`')
 
@@ -19,10 +21,7 @@ st.sidebar.subheader('Line chart parameters')
 plot_data = st.sidebar.multiselect('Select data', ['temp_min', 'temp_max'], ['temp_min', 'temp_max'])
 plot_height = st.sidebar.slider('Specify plot height', 200, 500, 250)
 
-st.sidebar.markdown('''
----
-Created with ❤️ by [Data Professor](https://youtube.com/dataprofessor/).
-''')
+
 
 
 # Row A
@@ -46,7 +45,7 @@ with c1:
     y_unit='day',
     color=time_hist_color,
     aggregate='median',
-    legend=None,
+    legend='none',
     height=345,
     use_container_width=True)
 with c2:
